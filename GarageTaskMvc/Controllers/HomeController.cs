b8a6670e1b92bd8a39aa305eac3bf08a221c865c
Nasir <imamaliyevnasir@gmail.com>
2023-10-26 10:44:04 +0400

    Add project files.

---
+++ GarageTaskMvc/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using GarageTaskMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GarageTaskMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            List<Product> products = new List<Product>
+            {
+                new Product
+                {
+                    Id = 1,
+                    Name = "Samsung",
+                    Count = 2,
+                    Price= 650
+                },
+                new Product
+                {
+                    Id = 2,
+                    Name = "Iphone",
+                    Count = 1,
+                    Price= 950
+                },
+                new Product
+                {
+                    Id = 3,
+                    Name = "Xiaomi",
+                    Count = 3,
+                    Price= 550
+                }
+            };
+            return View(products);
+        }
+    }
+}
---
+++ GarageTaskMvc/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace GarageTaskMvc.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Count { get; set; }
+        public double Price { get; set; }
+    }
+}
